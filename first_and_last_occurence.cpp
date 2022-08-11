23a6745bb1328d2a0b344d81be762eb7b157f00c
Error404 <81263628+XT-Error406@users.noreply.github.com>
2022-08-11 19:27:37 +0530

    Create first_and_last_occurence.cpp

---
+++ first_and_last_occurence.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+     
+   int lastoccurence(vector<int>arr,int n ,int target){
+    int start=0;
+    int end=n-1;
+    int ans=-1;
+    int mid=start+(end-start)/2;
+
+    while(start<=end){
+        
+        if(arr[mid]==target){
+            ans=mid;
+           start=mid+1;
+            }
+      else if(arr[mid]<target){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+        mid=start+(end-start)/2;
+     } 
+     return ans;
+
+}
+    
+int firstoccurence(vector<int>arr,int n,int target ){
+    int start=0;
+    int end=n-1;
+    int ans=-1;
+    int mid=start+(end-start)/2;
+
+    while(start<=end){
+        
+        if(arr[mid]==target){
+            ans=mid;
+            end=mid-1;
+            }
+      else if(arr[mid]<target){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+        mid=start+(end-start)/2;
+     } 
+     return ans;
+}
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+         int n=nums.size();
+        return{firstoccurence(nums,n,target),lastoccurence(nums,n,target)};
+    }
+};
